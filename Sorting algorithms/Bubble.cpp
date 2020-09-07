c8d16d542699d59a3c0ddaff9c5400bf8f869a67
vinayakkokane <51049381+vinayakkokane@users.noreply.github.com>
2020-09-07 23:56:44 +0530

    Add files via upload

---
+++ Sorting algorithms/Bubble.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubleSort(int arr[], int n)
+{
+    for(int i = 0; i < n-1; i++)
+    {
+        for(int j = 0; j < n-1-i; j++)
+        {
+            if(arr[j] > arr[j+1])
+                swap(arr[j],arr[j+1]);
+        }
+    }
+}
+
+int main(){
+
+    int n;  
+    cout<<"Enter size: \n";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter an array: \n";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    bubleSort(arr,n);
+    cout<<"Sorted array is :\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+    
